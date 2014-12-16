71823e235c1c4f1f06ca84a62de5314365653e81
william <wwwy3y3@gmail.com>
2014-12-16 11:35:26 +0800

    add exclude feature

--- example/app.js
+++ example/app.js
@@ -14,7 +14,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(require('subdomain-static')('apps'));
+app.use(require('../')({ 
+    folder: 'apps'
+}));
+app.use(function (req, res, next) {
+
+    next();
+})
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-module.exports= function (folder) {
+module.exports= function (settints) {
 	return function (req, res, next) {
-		if(req.subdomains.length==0)
+
+		if(req.subdomains.length==0 || exclude(req.subdomains[0]))
 	      	return next();
 
-	    	req.url= '/'+folder+'/'+req.subdomains[0]+req.url;
+	    	req.url= '/'+settints.folder+'/'+req.subdomains[0]+req.url;
 	    	next();
 		}
+
+
+	function exclude (domain) {
+		if(!settints.exclude_subdomains)
+			return false;
+
+		return (settints.exclude_subdomains.indexOf(domain)>=0);
+	}
 }
