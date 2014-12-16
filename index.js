d32ed16b9a6ee83cfd77782e75ebfe88986c4aae
william <wwwy3y3@gmail.com>
2014-12-16 12:15:08 +0800

    add feature domain

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports= function (settints) {
 	return function (req, res, next) {
+		if(!appHost(req.hostname)) // if request hostname is not settings hostname
+			return next();
 
 		if(req.subdomains.length==0 || exclude(req.subdomains[0]))
 	      	return next();
@@ -14,5 +16,23 @@ module.exports= function (settints) {
 			return false;
 
 		return (settints.exclude_subdomains.indexOf(domain)>=0);
+	}
+
+	function appHost (hostname) {
+		if(!settints.hostname)
+			return true;
+
+		var hosts= hostname.split('.');
+		var domain= lastTwo(hosts);
+		return (domain==settints.hostname);
+	}
+
+	function lastTwo (arr) {
+		if(arr.length==0)
+			return null;
+		else if(arr.length<2)
+			return arr[0];
+		else
+			return arr[arr.length-2] + '.' + arr[arr.length-1]
 	}
 }
