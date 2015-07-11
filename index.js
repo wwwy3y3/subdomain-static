f90f19a3f5467911273620d3407308be7f54670e
william <wwwy3y3@gmail.com>
2015-07-12 00:35:36 +0800

    fix

--- index.js
+++ index.js
@@ -3,25 +3,6 @@ var _= require('lodash');
 
 /*
  get domain, subdomains, and hostname
-
- testing
- app1.test.canner.io
- subdomain: test, app1
- hostname: app1.test.canner.io
- domain: canner.io
-
- prod:
- app1.cannerapp.com
- subdomain: app1
- hostname: app1.cannerapp.com
- domain: cannerapp.com
-
- local:
- app1.localapp.host
- subdomain: app1 
- hostname: app1.localapp.host
- domain: localapp.host
-
 */
 module.exports= function (settints) {
 	return function (req, res, next) {
@@ -43,49 +24,20 @@ module.exports= function (settints) {
 		if(hosts){
 			req.url= '/'+settints.folder+'/'+hosts[0]+req.url;
 			return next();
-		}
-
-		// not from appHost, from a unknown host
-		// check in db, if settings.cnameLookup exist
-		appHost(req, res).then(function (hosts) {
-			if(!hosts)
+		}else{
+			// if it's apphost with no appUrl
+			if(settints.hostname==domain)
 				return next();
 
-
-		    req.url= '/'+settints.folder+'/'+hosts.appUrl+req.url;
-		    next();	
-		}).catch(next);
-	}
-
-	function appHost (req, res) {
-		var hostname= req.hostname;
-		if(!settints.hostname)
-			return true;
-
-		var length= settints.hostname.split('.').length;
-		var hosts= hostname.split('.');
-		var appUrl= (hosts.length>length)?hosts[0]:null;
-		var domain= getHost(hosts, length);
-		if(domain!==settints.hostname){
-			// unknown domain, who r u?
+			// who is this unknown hostname?
 			if(!settints.cnameLookup)
-				return Q(false);
-			else  // hey! it's me! lookup in your db
-				return Q.when(settints.cnameLookup(domain, req, res)).then(function (url) {
-					return { appUrl: url };
-				});
-		}else{
-			// yeah, same hostname, you go!
-			return Q({ appUrl: appUrl, domain: domain });
+				return next();
+
+			// hey! it's me! lookup in your db
+			return Q.when(settints.cnameLookup(hostname, req, res)).then(function (url) {
+				req.url= '/'+settints.folder+'/'+url+req.url;
+				next();
+			});
 		}
 	}
-}
-
-var getHost= exports.getHost= function (hosts, length) {
-	if(hosts.length==0)
-		return null;
-	else if(hosts.length<2)
-		return hosts[0];
-	else
-		return hosts.slice(hosts.length-length).join('.');
 }
